9cd6a087bddea0037ce5165623401c7b99698295
Nayanayadav2001 <nayanayadav2001@gmail.com>
2024-03-05 14:16:43 +0530

    rs

---
+++ react-one/src/App.js
@@ -0,0 +1,17 @@
+import Header  from "./components/Header"
+import Footer from "./components/Footer"
+import Message from "./components/Message"
+
+
+function App(){
+
+  return <div>
+    <h1>App Component</h1>
+    <h2>A</h2>
+    <Header/>
+    <Footer/>
+    <Message/>
+  </div>
+
+}
+export default App
---
+++ react-one/src/components/Message.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Message extends React.Component{
+    render(){
+        return <div>
+            <h3>Message Component</h3>
+        </div>
+    }
+}
+
+export default Message
