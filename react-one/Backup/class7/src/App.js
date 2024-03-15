74c72292260cefd111a02f8f56711534d87c6c84
Nayanayadav2001 <nayanayadav2001@gmail.com>
2024-03-15 10:14:17 +0530

    rs

---
+++ react-one/Backup/class7/src/App.js
@@ -0,0 +1,17 @@
+import Employee from "./ListRendering/Employee"
+import Navbar from "./ListRendering/Navbar"
+
+
+
+function App(){
+
+    return (
+    <div>
+        <Navbar/>
+
+        <Employee/>
+    </div>
+    )
+}
+
+export default App
